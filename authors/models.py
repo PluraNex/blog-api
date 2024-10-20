2467cda0c0b936e83e9f6a41283c3ba82844554f
iuryeng <iuryfernandes.eng@gmail.com>
2024-10-20 13:10:22 -0300

    test: modulariza testes, aumenta cobertura, resolve issue sonar, ajusta enpoint boas praticas

--- authors/models.py
+++ authors/models.py
@@ -5,8 +5,8 @@ class Author(models.Model):
     """Modelo para autores de artigos, incluindo biografia e imagem opcional."""
 
     name = models.CharField(max_length=100)
-    biography = models.TextField(blank=True, null=True)
-    profession = models.CharField(max_length=100, blank=True, null=True)
+    biography = models.TextField(blank=True)
+    profession = models.CharField(max_length=100, blank=True)
     image = models.ImageField(upload_to="author_images/", blank=True, null=True)
 
     class Meta:
