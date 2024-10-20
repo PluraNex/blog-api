f81aee1354fcef6a57e227562c422cd4d83d0d10
iuryeng <iuryfernandes.eng@gmail.com>
2024-10-20 13:10:52 -0300

    reafactor: utiliza boas praticas e deixa codigo mais clean

--- authors/views.py
+++ authors/views.py
@@ -1,37 +1,31 @@
 # authors/views.py
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-from drf_yasg import openapi 
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from drf_yasg.utils import swagger_auto_schema
 from .models import Author
 from .serializers import AuthorSerializer
-from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-class AuthorDetailView(APIView):
+
+class AuthorDetailView(generics.RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'id'
+    lookup_url_kwarg = 'author_id'
+
     @swagger_auto_schema(
         operation_summary="Get author details",
         operation_description="Retrieve detailed information about a specific author.",
         responses={
             200: openapi.Response(
                 description="Detailed information about the author",
-                schema=AuthorSerializer(),
+                schema=AuthorSerializer()
             ),
             404: openapi.Response(description="Author not found"),
-            500: openapi.Response(description="Internal server error"),
         },
         tags=['Authors']
     )
-    def get(self, request, author_id):
-        try:
-            author = Author.objects.get(id=author_id)
-            serializer = AuthorSerializer(author)
-            return Response(serializer.data)
-        except Author.DoesNotExist:
-            return Response(
-                {"error": "Author not found"}, status=status.HTTP_404_NOT_FOUND
-            )
-        except Exception as e:
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
